54c62e372009efdbc372f43a80789d30b1940eba
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2024-03-08 09:07:40 +0100

    Initial commit created using Spring Initializr

---
+++ src/main/java/ml/crac/CracApplication.java
@@ -0,0 +1,13 @@
+package ml.crac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CracApplication.class, args);
+	}
+
+}
